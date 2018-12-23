18ab4e450cc1f9e352b5f371552af33762c9a030
Andrey2470T <andreyt2203@gmail.com>
2018-12-23 23:30:03 +0300

    Changed a few blend files

--- redecorating.lua
+++ redecorating.lua
@@ -13,3 +13,29 @@ minetest.register_node("luxury_decor:laminate", {
     sounds = default.node_sound_leaves_defaults()
 })
     
+minetest.register_node("luxury_decor:simple_flowerpot", {
+    description = "Simple Flowerpot",
+    visual_scale = 0.5,
+    mesh = "simple_flowerpot.obj",
+    tiles = {"simple_flowerpot.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {choppy = 2},
+    drawtype = "mesh",
+    collision_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+            --[[{-0.65, -0.3, -1.46, 0.65, 1.4, -1.66},
+            {-0.65, -0.3, 0.46, 0.65, 1.4, 0.66}]]
+        }
+    },
+    selection_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5}
+        }
+    },
+    sounds = default.node_sound_wood_defaults()
+})
+    
